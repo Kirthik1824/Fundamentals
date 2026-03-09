bb55fee58ec48527830f86643a0bf8a6414ac871
Kirthik <kirthik.nitt@gmail.com>
2026-03-09 22:15:46 +0530

    note-model

--- MERN/backend/src/server.js
+++ MERN/backend/src/server.js
@@ -1,14 +1,18 @@
 import express from "express";
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
+const port = process.env.PORT || 5000;
 
 connectDB();
 
 app.use("/api/notes", notesRoutes);
 
-app.listen(5000, () => {
-    console.log("Server is running on port 5000");
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 })
-
+ 
