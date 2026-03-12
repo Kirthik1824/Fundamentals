bc30aac2ea25e6d281a1ef29bf83c8139c123c88
Kirthik <kirthik.nitt@gmail.com>
2026-03-12 19:26:44 +0530

    delete-functionality

--- MERN/thinkboard/frontend/src/pages/HomePage.jsx
+++ MERN/thinkboard/frontend/src/pages/HomePage.jsx
@@ -40,10 +40,14 @@ const HomePage = () => {
     <div className=' max-w-7xl mx-auto p-4 mt-6'>
       {loading && <div className='text-center text-primary py-10'>Loading notes...</div>}
 
+      {!loading && notes.length === 0 && !isRateLimited && (
+        <div className='text-center text-primary py-10'>No notes found. Create your first note!</div>
+      )}
+
       {notes.length > 0 && !isRateLimited && (
         <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
           {notes.map(note => (
-            <NoteCard key={note._id} note={note} />
+            <NoteCard key={note._id} note={note} setNotes={setNotes} />
           ))}
         </div>
       )}
